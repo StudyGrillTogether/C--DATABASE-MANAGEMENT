ea99237484104baf0f1efbb9da7d564576cf38b0
Kshitij1ghk <kshitijwk@gmail.com>
2026-03-02 15:37:02 +0530

    Add project files.

---
+++ DAY-1-GIT-EXERCISE/DAY-1-GIT-EXERCISE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAY_1_GIT_EXERCISE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
